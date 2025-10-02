e8f9c00b818377c367412b465194205dd3732618
ksy050131 <154244290+ksy050131@users.noreply.github.com>
2025-10-02 20:09:01 +0900

    [Silver III] Title: N과 M (1), Time: 172 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -0,0 +1,36 @@
+import sys
+
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+numlist = [i for i in range(1, n + 1)]
+numlist.sort()
+
+elem = []
+elemlist = []
+visited = n * [False]
+
+# print(visited)
+
+def dfs():
+    if len(elem) == m:
+        print(*elem)
+        return
+
+    prev = None
+
+    for i in range(n):
+        if visited[i]: continue
+        if numlist[i] == prev: continue # 같은 깊이에서 중복 제거
+
+        visited[i] = True
+        
+        elem.append(numlist[i])
+        # print(f"index: {i}, visited: {visited[i]}") 디버깅
+        dfs()
+        elem.pop()
+
+        visited[i] = False
+        prev = numlist[i]
+
+dfs()
