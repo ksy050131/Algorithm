fc73055e3b3583ead7fff8aef0685e5ae053e468
ksy050131 <154244290+ksy050131@users.noreply.github.com>
2025-10-04 01:00:01 +0900

    [Silver III] Title: N과 M (3), Time: 1880 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -0,0 +1,23 @@
+import sys
+
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+
+visited = (n + 1) * [False]
+elem = []
+# elemlist = []
+
+def dfs(depth: int):
+    if depth == m:
+        print(*elem)
+        # elemlist.append(' '.join(map(str, elem)))
+        return
+
+    for i in range(1, n + 1):
+        elem.append(i)
+        dfs(depth + 1)
+        elem.pop()
+
+dfs(0)
+# print('\n'.join(elemlist))
